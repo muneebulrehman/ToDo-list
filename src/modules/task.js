80f178d345f2f48bc9052b8ec48ca08e91fce2d1
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-02-17 13:38:03 +0530

    Added tickmark and clear completed functionality

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,35 +1,40 @@
 /* eslint-disable */
-import {
-  renderList,
-} from './renderView.js';
+import { renderList } from './renderView.js';
 /* eslint-enable */
 class Task {
 	tasks = [];
 
 	getTasks = () => {
-	  if (localStorage.getItem('tasks')) this.tasks = JSON.parse(localStorage.getItem('tasks'));
+		if (localStorage.getItem('tasks'))
+			this.tasks = JSON.parse(localStorage.getItem('tasks'));
 	};
 
 	setTask = () => {
-	  localStorage.setItem('tasks', JSON.stringify(this.tasks));
-	  this.getTasks();
+		localStorage.setItem('tasks', JSON.stringify(this.tasks));
+		this.getTasks();
 	};
 
 	resetId = (num) => {
-	  /* eslint-disable */
-	  this.tasks.forEach((task) => {
-	    if (task.index > num) task.index--;
-	  });
-	  this.setTask();
-	  this.getTasks();
+		/* eslint-disable */
+		this.tasks.forEach((task) => {
+			if (task.index > num) task.index--;
+		});
+		this.setTask();
+		this.getTasks();
 		renderList();
 		/* eslint-enable */
+	};
+	revalueate = () => {
+		const length = this.tasks.length;
+		this.tasks.forEach((el, i) => {
+			el.index = i + 1;
+		});
+		this.setTask();
+		renderList();
 	};
 }
 
 const newTask = new Task();
 /* eslint-disable */
-export {
-  newTask,
-};
+export { newTask };
 /* eslint-enable */
---
+++ src/modules/tickMark.js
@@ -0,0 +1,27 @@
+import { list } from '../index.js';
+import { newTask } from './task.js';
+import { renderList } from './renderView.js';
+
+const tickMark = () => {
+	list.addEventListener('click', (e) => {
+		if (e.target.classList.contains('checkbox')) {
+			const targetEl = e.target;
+			const num = e.target.dataset.id;
+			if (targetEl.checked) newTask.tasks[num - 1].completed = true;
+			else newTask.tasks[num - 1].completed = false;
+			newTask.setTask();
+			renderList();
+		}
+	});
+};
+
+const clearBtn = document.querySelector('.clear-btn');
+const clearCompleted = () => {
+	clearBtn.addEventListener('click', () => {
+		newTask.tasks = newTask.tasks.filter((el) => el.completed !== true);
+		newTask.setTask();
+		newTask.revalueate();
+	});
+};
+
+export { tickMark, clearCompleted };
